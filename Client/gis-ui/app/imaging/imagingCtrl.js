f562e05c23897301a03ceac28be6ef41dd033165
avik1q <avik1q@gmail.com>
2016-05-23 15:25:23 +0300

    2nd update for imaging team
    #Earth Quake page#
    validate the magnetite no more then 10 and not below then 1
    
    #crime page#
    fix headline 
    add more api (2)
    
    #weather page#
    add panel for show the weather in 1<x<16 days
    add input for select days to forecasting (spinner)
    
    #general#
    upper button is colored when selected

--- Client/gis-ui/app/imaging/imagingCtrl.js
+++ Client/gis-ui/app/imaging/imagingCtrl.js
@@ -45,6 +45,7 @@ function imagingCtrl() {
         chooseProb=1;
         $('#radiusSelect').val(100);
         $('#magnetiteSelect').val(7);
+        $('#forecastingdaysselect').val(5);
         map = L.map('map').setView([32.111767, 34.801361], 13);
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -54,7 +55,9 @@ function imagingCtrl() {
     $('#earthquakebtn').on('click',function(){ //reset map & show map and more
         $('#radiusSelect').val(100);
         $('#magnetiteSelect').val(6.8);
+
         allFormClose();
+        $('#earthquakebtn').addClass("btn-primary")
         document.getElementById('earthquakeForm').style.display= 'block';
         document.getElementById('map').style.display= 'block';
         map.remove();
@@ -70,12 +73,18 @@ function imagingCtrl() {
 
     $('#crimebtn').on('click',function(){ //reset map & show map and more
         allFormClose();
+        $('#crimebtn').addClass("btn-primary");
+        $('#mapSelect').val(1);
+        document.getElementById('youmap').style.display= 'block';
         document.getElementById('crime').style.display= 'block';
         document.getElementById('map').style.display= 'none';
-    });// delete all map layer & reset map for EQ
+        
+    });// delete all map layer & reset map for crime
 
     $('#weatherbtn').on('click',function(){ //reset map & show map and more
         allFormClose();
+        $('#weatherbtn').addClass("btn-primary");
+        $('#ForecastingDaysSelect').val(5);
         document.getElementById('weatherForm').style.display= 'block';
         document.getElementById('map').style.display= 'block';
         map.remove();
@@ -90,15 +99,57 @@ function imagingCtrl() {
         });
     });// delete all map layer & reset map for weather
 }
-
+function selectapi() {
+    var x = document.getElementById("mapSelect").value;
+    closeAllApi();
+    console.log(x);
+    switch (x)
+    {
+        case "1":
+        {
+            document.getElementById('youmap').style.display= 'block';
+            break;
+        }
+        case "2":
+        {
+            document.getElementById('crimereports').style.display= 'block';
+            break;
+        }
+        case "3":
+        {
+            document.getElementById('spotcrime').style.display= 'block';
+            break;
+        }   
+        default:
+            console.log(x);
+    }
+}
+function closeAllApi() {
+    document.getElementById('youmap').style.display= 'none';
+    document.getElementById('crimereports').style.display= 'none';
+    document.getElementById('spotcrime').style.display= 'none';
+}
 function allFormClose() {
+    $('#weatherbtn').removeClass("btn-primary");
+    $('#crimebtn').removeClass("btn-primary");
+    $('#earthquakebtn').removeClass("btn-primary");
+    //$('#weatherbtn').addClass("btn-default");
+    //$('#crimebtn').addClass("btn-default");
+    //$('#earthquakebtn').addClass("btn-default");
     document.getElementById('earthquakeForm').style.display= 'none';
     document.getElementById('weatherForm').style.display= 'none';
     document.getElementById('crime').style.display= 'none'
 }
 function loadXMLDocforWH(map,e) {
     var loc;
+    var loce;
+    var selectForecastingDays=$('#ForecastingDaysSelect').val();
+    if (selectForecastingDays < 1 || selectForecastingDays > 16) {
+        window.alert("number must beetwean 1 to 16");
+        return;
+    }
     var u = "http://api.openweathermap.org/data/2.5/weather?APPID=3976c02a714fa412bed57f7a86ebfae1&lat=" + e.latlng.lat + "&lon=" + e.latlng.lng;
+    var u2= "http://api.openweathermap.org/data/2.5/forecast/daily?appid=3976c02a714fa412bed57f7a86ebfae1&lat=" + e.latlng.lat + "&lon=" + e.latlng.lng+"&cnt="+selectForecastingDays+"&mode=json"
     $.ajax({
         url: u,
         dataType: 'jsonp',
@@ -106,7 +157,7 @@ function loadXMLDocforWH(map,e) {
             withCredentials: true
         }
     }).done(function (msg) {
-        console.log(msg);
+        //console.log(msg);
         loc = msg;//save the info for load the map and the marker
 
         //load map after read the api
@@ -118,11 +169,38 @@ function loadXMLDocforWH(map,e) {
             .bindPopup('weather in ' + loc.name + ' is ' + loc.weather[0].description)// text on popup
             .openPopup();
     });
+    $.ajax({
+        url: u2,
+        dataType: 'jsonp',
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function (msg) {
+
+       //console.log(msg);
+        loce = msg;//save the info for load the map and the marker
+
+        //panel map after read the api
+        document.getElementById("titlePanel").innerHTML ="Forecasting weather is "+loce.city.name;
+
+        var str="";
+        for(var i=0;i<selectForecastingDays;i++)
+        {
+
+            str+="day "+(i+1)+": wather "+loce.list[i].weather[0].main+"<br>";
+        }
+        document.getElementById("panelbody").innerHTML =str;
+
+    });
 }
 function loadXMLDocforEQ(map,e) {
     var loc;
     var selectedText = $('#radiusSelect').find("option:selected").text();
     var selectmag=$('#magnetiteSelect').val();
+    if (selectmag < 1 || selectmag > 10) {
+        window.alert("number must beetwean 1 to 10");
+        return;
+    }
     var u = "http://api.openhazards.com/GetEarthquakeProbability?q=" + e.latlng.lng + "%2C" + e.latlng.lat + "&m=" + selectmag + "&r=" + selectedText;
     $.ajax({
         url: u,
